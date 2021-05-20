a236f8b6adf8a6bb6ef4bac434a2e652c5d1c035
Carlos Henrique <54608090+carlos0406@users.noreply.github.com>
2021-05-19 23:36:00 -0300

    #00 inicializando repositorio

--- README.md
+++
@@ -1,2 +0,0 @@
-# projeto_base_nodejs
-Modelo padrão de projeto nodejs usando typeScript,eslint para padronizar codigo, babel para build,e jest para testes
--- src/tests/Simpletest.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SimpleModel } from '@models/SimpleModel'
-test('it shold be ok', () => {
-  const user = new SimpleModel()
-  user.propiedade = 'propiedade'
-
-  expect(user.propiedade).toEqual('propiedade')
-})
