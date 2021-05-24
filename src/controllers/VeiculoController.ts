9e8601597cdb932e988cadf149c4f8f047f8f7ee
Carlos Henrique <54608090+carlos0406@users.noreply.github.com>
2021-05-23 23:36:37 -0300

    update veiculo

--- src/controllers/VeiculoController.ts
+++ src/controllers/VeiculoController.ts
@@ -18,7 +18,7 @@ export class VeiculoController {
   }
 
   async basic (request: Request, response: Response) {
-    return response.json({ message: 'Olá, AWS' })
+    return response.json({ message: 'Olá, AWS 2 2' })
   }
 
   async remove (request: Request, response: Response) {
